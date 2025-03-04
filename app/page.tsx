bd407feff972a5b7529bdf09941613e8002f268e
Cybersport <Cybersport@26kadr.internal>
2025-03-04 12:30:37 +0300

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Container, Header, Title } from "@/components/shared"
+import { Filters } from "@/components/shared/filters";
 import { TopBar } from "@/components/shared/top-bar";
 
 
@@ -11,6 +12,14 @@ export default function Home() {
     <Title  text="Все пиццы" className='font-extrabold' size='lg'/>
 </Container>
 <TopBar/>
+<Container className="mt-10 pb-14">
+<div className="flex gap-[60px]"></div>
+  <div className="w-[250px]"></div>
+  <Filters/>
+
+<div className="flex-1"></div>
+<div className="flex col gap-16">Список товаров</div>
+</Container>
 </>
   );
 }
